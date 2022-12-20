e34e5d48fe518abb71da5d17ae5fc6e4cd39ea02
Tallurilikitha2004 <121090004+Tallurilikitha2004@users.noreply.github.com>
2022-12-20 14:43:01 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,13 @@
+import java.io.*;
+ import java.lang.*;
+class table
+{
+  public static void main(String args[])
+  {
+
+  for(int i=1;i<10;i++)
+  {
+    System.out.println("2*"+i+"=" +2*i);
+  }
+}
+}
